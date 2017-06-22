972e172390946f29c1cf086b23b5688a8bf70fea
louca75 <louca.mendy@gmail.com>
2017-06-22 11:51:08 +0200

    comit louca branche

--- Logos_Data/src/main/java/com/logos/test/TestCreationBDD.java
+++ Logos_Data/src/main/java/com/logos/test/TestCreationBDD.java
@@ -4,13 +4,17 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.logos.data.connexion.api.IDaoConnexionUser;
+import com.logos.entity.user.Utilisateur;
+
 public class TestCreationBDD {
 
 	private static Logger log = Logger.getLogger(TestCreationBDD.class);
 	public static void main(String[] args) {
 		BeanFactory bf = new ClassPathXmlApplicationContext("classpath:springData.xml");
 		
-
+		IDaoConnexionUser dao = bf.getBean(IDaoConnexionUser.class);
+		log.debug(dao.checkLoginPassword("loulou75", "loulou75"));
 	}
 
 }
