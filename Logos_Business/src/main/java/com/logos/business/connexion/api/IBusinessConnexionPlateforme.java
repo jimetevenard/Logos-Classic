0ec2675bbcc71f2e4f307f72568e52f56bd8bb13
victorgl <victor85gl@hotmail.com>
2017-06-22 14:36:42 +0200

    service!

--- Logos_Business/src/main/java/com/logos/business/connexion/api/IBusinessConnexionPlateforme.java
+++ Logos_Business/src/main/java/com/logos/business/connexion/api/IBusinessConnexionPlateforme.java
@@ -1,9 +1,12 @@
 package com.logos.business.connexion.api;
 
+import org.springframework.stereotype.Service;
+
 import com.logos.entity.user.Eleve;
 import com.logos.entity.user.Professeur;
 import com.logos.entity.user.Utilisateur;
 
+@Service
 public interface IBusinessConnexionPlateforme {
 	Eleve inscrireEleve(Eleve eleve);
 	Professeur inscrireProfesseur(Professeur professeur);
