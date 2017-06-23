dc450dd34a413100450ad67e9b5d6990dcf75c37
priscillia78 <priscillia.prehaud@outlook.com>
2017-06-23 16:22:08 +0200

    Inscription

--- Logos_Front/src/main/java/com/logos/front/inscription/bean/InsciptionManagedBean.java
+++ Logos_Front/src/main/java/com/logos/front/inscription/bean/InsciptionManagedBean.java
@@ -2,18 +2,22 @@ package com.logos.front.inscription.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 
 import org.hibernate.exception.ConstraintViolationException;
 
 import com.logos.business.connexion.api.IBusinessConnexionPlateforme;
 import com.logos.entity.user.Eleve;
+import com.logos.front.connexion.LoginMB;
 
 @ManagedBean(name="mbInscription")
-@ViewScoped
+@SessionScoped
 public class InsciptionManagedBean {
 	@ManagedProperty(value="#{businessConnexionPlateforme}")
 	private IBusinessConnexionPlateforme bu;
+	
+	@ManagedProperty(value="#{loginMB}")
+	private LoginMB logMb;
 	
 	private String nom;
 	private String prenom;
@@ -24,18 +28,25 @@ public class InsciptionManagedBean {
 	private String violationMailLogin;
 	
 	public String ajouterEleve(){
-		
+		String redirect = null;
+		Eleve eleve = null;
 		try{
-			Eleve e = new Eleve(null, nom, prenom, login, password, mail);
-			bu.inscrireEleve(e);
-			return "/accueil_eleve.xhtml?faces-redirect=true";
-
+			System.out.println("passes tu là ?");
+			eleve = new Eleve(null, nom, prenom, login, password, mail);
+			bu.inscrireEleve(eleve);		
 		} catch (ConstraintViolationException e) {
 			violationMailLogin = "Mail ou login déjà utilisé";
 		} catch (Exception e2) {
 			// TODO: autre erreur
+		} finally {
+			System.out.println(logMb == null);
+			logMb.setUserConnected(eleve);
+			System.out.println("après");
+			logMb.setLoggedIn(true);
+			
+			redirect = "/accueil_eleve.xhtml?faces-redirect=true";
 		}
-		return null;
+		return redirect;
 	}
 
 	public IBusinessConnexionPlateforme getBu() {
@@ -100,6 +111,14 @@ public class InsciptionManagedBean {
 
 	public void setViolationMailLogin(String violationMailLogin) {
 		this.violationMailLogin = violationMailLogin;
+	}
+
+	public LoginMB getLogMb() {
+		return logMb;
+	}
+
+	public void setLogMb(LoginMB logMb) {
+		this.logMb = logMb;
 	}
 	
 	
