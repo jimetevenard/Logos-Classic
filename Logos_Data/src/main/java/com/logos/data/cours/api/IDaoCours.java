7a4a29b56c8b4681213921973a8cb580d8295617
louca75 <louca.mendy@gmail.com>
2017-06-23 16:46:27 +0200

    interface cours

---
+++ Logos_Data/src/main/java/com/logos/data/cours/api/IDaoCours.java
@@ -0,0 +1,21 @@
+package com.logos.data.cours.api;
+
+import java.util.List;
+
+import com.logos.entity.cours.Categorie;
+import com.logos.entity.cours.Cours;
+import com.logos.entity.cours.Langue;
+import com.logos.entity.cours.Niveau;
+
+public interface IDaoCours {
+	
+	Cours addCours(Cours cours);
+	Cours updateCours(Cours cours);
+	boolean deleteCours(Cours cours);
+	Cours getCoursByid(Integer id);
+	List<Cours> getCoursByLangue(Langue langue);
+	List<Cours> getCoursByCategorie(Categorie categorie);
+	List<Cours> getCoursByNiveau(Niveau niveau);
+	Double getNoteMoyenneByCours(Cours cours);
+	
+}
