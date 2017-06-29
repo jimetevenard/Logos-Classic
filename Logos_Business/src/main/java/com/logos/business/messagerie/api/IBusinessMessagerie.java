72c39809f582aa4cc3115534ea7707dd1ac22cda
etinland@gmail.com <etinland@gmail.com>
2017-06-29 14:35:56 +0200

    commit dao bu eval encore

---
+++ Logos_Business/src/main/java/com/logos/business/messagerie/api/IBusinessMessagerie.java
@@ -0,0 +1,5 @@
+package com.logos.business.messagerie.api;
+
+public interface IBusinessMessagerie {
+
+}
---
+++ Logos_Business/src/main/java/com/logos/business/messagerie/impl/BusinessMessagerie.java
@@ -0,0 +1,10 @@
+package com.logos.business.messagerie.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.logos.business.messagerie.api.IBusinessMessagerie;
+
+@Service
+public class BusinessMessagerie implements IBusinessMessagerie {
+
+}
