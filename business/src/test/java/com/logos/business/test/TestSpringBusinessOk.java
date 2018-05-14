8ce23c45602d1185fbb5e9953e50822123d862ce
Jim Etevenard <jimetevenard@gmail.com>
2018-05-14 12:02:51 +0200

    test unitaire Spring OK

---
+++ business/src/test/java/com/logos/business/test/TestSpringBusinessOk.java
@@ -0,0 +1,24 @@
+package com.logos.business.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.logos.business.connexion.api.IBusinessConnexionPlateforme;
+
+
+public class TestSpringBusinessOk {
+	
+	@Test
+	public void dataOk() {
+		BeanFactory bf = new ClassPathXmlApplicationContext("classpath:springBusiness.xml");
+		
+		IBusinessConnexionPlateforme dao = bf.getBean(IBusinessConnexionPlateforme.class);
+		
+		assertNotNull(dao);			
+		
+	}
+}
